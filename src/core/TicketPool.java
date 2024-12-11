f6ea736ba6405bff16c7ba7bf2eb8390f5e77ab9
harishan-avinesh <avineshharishan@gmail.com>
2024-12-11 23:38:32 +0530

    again added reentrant lock

--- src/core/TicketPool.java
+++ src/core/TicketPool.java
@@ -1,3 +1,102 @@
+
+package core;
+import systemconfig.SystemConfig;
+import utility.LoggerConfig;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.*;
+
+public class TicketPool {
+    private final List<String> tickets = new LinkedList<>();
+    private final int maxCapacity;
+    private int totalTicketsAdded = 0;
+    private final int totalTickets;
+    private int ticketsSold = 0;
+
+    private final Lock lock = new ReentrantLock(); // Lock for controlling access
+    private final Condition notFull = lock.newCondition(); // Condition for vendors
+    private final Condition notEmpty = lock.newCondition(); // Condition for customers
+
+    private static final Logger logger = LoggerConfig.getLogger(TicketPool.class); // Using LoggerConfig to initialize logger
+
+    public TicketPool(SystemConfig config) {
+        this.maxCapacity = config.getMaxTicketCapacity();
+        this.totalTickets = config.getTotalTickets();
+    }
+
+    // Method for vendors to add tickets in a batch
+    public boolean addTickets(List<String> batchTickets) throws InterruptedException {
+        lock.lock(); // Acquire the lock
+        try {
+            // Wait if the pool is too full to accommodate the batch
+            while (tickets.size() + batchTickets.size() > maxCapacity) {
+                notFull.await(); // Vendor waits until there is space
+            }
+
+            // Add tickets in batch
+            for (String ticket : batchTickets) {
+                if (totalTicketsAdded >= totalTickets) {
+                    return false; // Stop adding tickets if the total limit is reached
+                }
+                tickets.add(ticket); // Add ticket to the pool
+                totalTicketsAdded++;
+                logger.info(String.format("Ticket Added: %s | Pool Size: %d | Total Tickets Added: %d",
+                        ticket, tickets.size(), totalTicketsAdded));
+                //System.out.println("Ticket added: " + ticket + " ( Pool size: " + tickets.size() + " )" + "Ticket No. " + totalTicketsAdded);
+            }
+
+            notEmpty.signalAll(); // Notify customers that tickets are available
+            return true;
+
+        } finally {
+            lock.unlock(); // Release the lock
+        }
+    }
+
+    // Method for customers to remove tickets
+    public String removeTicket() throws InterruptedException {
+        lock.lock(); // Acquire the lock
+        try {
+            // Wait if the pool is empty
+            while (tickets.isEmpty()) {
+                notEmpty.await(); // Customer waits until a ticket is available
+            }
+
+            String ticket = tickets.remove(0); // Remove ticket from the pool
+            ticketsSold++;
+
+            logger.info(String.format("Ticket Purchased: %s | Pool Size: %d | Tickets Sold: %d | Total Tickets Left: %d",
+                    ticket, tickets.size(), ticketsSold, totalTickets - ticketsSold));
+            //System.out.println("Ticket purchased: " + ticket + " ( Pool size: " + tickets.size() + " )" + "Ticket No. " + (totalTicketsAdded - tickets.size())) ;
+
+            notFull.signalAll(); // Notify vendors that space is available
+            return ticket;
+        } finally {
+            lock.unlock(); // Release the lock
+        }
+    }
+
+    // Getter for current pool size (optional for monitoring purposes)
+    public int getCurrentSize() {
+        return tickets.size();
+    }
+
+    public int getTotalTicketsAdded() {
+        return totalTicketsAdded;
+    }
+
+    public int getTicketsSold(){
+        return ticketsSold;
+    }
+
+}
+
+/*
 package core;
 
 import systemconfig.SystemConfig;
@@ -90,98 +189,4 @@ public class TicketPool {
     }
 }
 
-/*package core;
-import systemconfig.SystemConfig;
-import utility.LoggerConfig;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.locks.Condition;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
-import java.util.logging.*;
-
-public class TicketPool {
-    private final List<String> tickets = new LinkedList<>();
-    private final int maxCapacity;
-    private int totalTicketsAdded = 0;
-    private final int totalTickets;
-    private int ticketsSold = 0;
-
-    private final Lock lock = new ReentrantLock(); // Lock for controlling access
-    private final Condition notFull = lock.newCondition(); // Condition for vendors
-    private final Condition notEmpty = lock.newCondition(); // Condition for customers
-
-    private static final Logger logger = LoggerConfig.getLogger(TicketPool.class); // Using LoggerConfig to initialize logger
-
-    public TicketPool(SystemConfig config) {
-        this.maxCapacity = config.getMaxTicketCapacity();
-        this.totalTickets = config.getTotalTickets();
-    }
-
-    // Method for vendors to add tickets in a batch
-    public boolean addTickets(List<String> batchTickets) throws InterruptedException {
-        lock.lock(); // Acquire the lock
-        try {
-            // Wait if the pool is too full to accommodate the batch
-            while (tickets.size() + batchTickets.size() > maxCapacity) {
-                notFull.await(); // Vendor waits until there is space
-            }
-
-            // Add tickets in batch
-            for (String ticket : batchTickets) {
-                if (totalTicketsAdded >= totalTickets) {
-                    return false; // Stop adding tickets if the total limit is reached
-                }
-                tickets.add(ticket); // Add ticket to the pool
-                totalTicketsAdded++;
-                logger.info(String.format("Ticket Added: %s | Pool Size: %d | Total Tickets Added: %d",
-                        ticket, tickets.size(), totalTicketsAdded));
-                //System.out.println("Ticket added: " + ticket + " ( Pool size: " + tickets.size() + " )" + "Ticket No. " + totalTicketsAdded);
-            }
-
-            notEmpty.signalAll(); // Notify customers that tickets are available
-            return true;
-
-        } finally {
-            lock.unlock(); // Release the lock
-        }
-    }
-
-    // Method for customers to remove tickets
-    public String removeTicket() throws InterruptedException {
-        lock.lock(); // Acquire the lock
-        try {
-            // Wait if the pool is empty
-            while (tickets.isEmpty()) {
-                notEmpty.await(); // Customer waits until a ticket is available
-            }
-
-            String ticket = tickets.remove(0); // Remove ticket from the pool
-            ticketsSold++;
-
-            logger.info(String.format("Ticket Purchased: %s | Pool Size: %d | Tickets Sold: %d | Total Tickets Left: %d",
-                    ticket, tickets.size(), ticketsSold, totalTickets - ticketsSold));
-            //System.out.println("Ticket purchased: " + ticket + " ( Pool size: " + tickets.size() + " )" + "Ticket No. " + (totalTicketsAdded - tickets.size())) ;
-
-            notFull.signalAll(); // Notify vendors that space is available
-            return ticket;
-        } finally {
-            lock.unlock(); // Release the lock
-        }
-    }
-
-    // Getter for current pool size (optional for monitoring purposes)
-    public int getCurrentSize() {
-        return tickets.size();
-    }
-
-    public int getTotalTicketsAdded() {
-        return totalTicketsAdded;
-    }
-
-    public int getTicketsSold(){
-        return ticketsSold;
-    }
-
-}
-*/
+ */
